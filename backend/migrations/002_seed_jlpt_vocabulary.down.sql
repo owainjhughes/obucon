cc8df9345411ed2317dc2b659bac6a6b8bd16405
owainjhughes <owainjhughes@gmail.com>
2026-02-20 16:14:14 +0000

    chore: removed big psql files from git; need to be undone for aws deployment

--- backend/migrations/002_seed_jlpt_vocabulary.down.sql
+++
@@ -1,4 +0,0 @@
--- Rollback: Remove seeded JLPT vocabulary
--- Only deletes pre-populated dictionary entries (jlpt_level IS NOT NULL)
-
-DELETE FROM japanese_dictionary WHERE jlpt_level IS NOT NULL;
